749ffa0bd286a1059537f103133214120c4cac1d
Ibrahim-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-07-21 19:50:22 +0200

    Updates

---
+++ precache-manifest.1f1ffa8e3f3c861971a619fc029e4c14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "77fc7bd3fcb0e3ed949a",
+    "url": "/translatorkit/static/css/main.b6a9b868.chunk.css"
+  },
+  {
+    "revision": "77fc7bd3fcb0e3ed949a",
+    "url": "/translatorkit/static/js/main.77fc7bd3.chunk.js"
+  },
+  {
+    "revision": "69358c018fdfb9db9b41",
+    "url": "/translatorkit/static/js/1.69358c01.chunk.js"
+  },
+  {
+    "revision": "dd9386de44925766f39b",
+    "url": "/translatorkit/static/js/runtime~main.dd9386de.js"
+  },
+  {
+    "revision": "238a9f0c2768b3a607358b4eeed523f6",
+    "url": "/translatorkit/static/media/arrow.238a9f0c.svg"
+  },
+  {
+    "revision": "3b168bbe8d20132cd729a1cef8b8ff19",
+    "url": "/translatorkit/static/media/black.3b168bbe.svg"
+  },
+  {
+    "revision": "93a54c1f24710c2f35591d9db6ae1b27",
+    "url": "/translatorkit/static/media/white.93a54c1f.svg"
+  },
+  {
+    "revision": "9d558938bdf831d115d655eeb4d81d10",
+    "url": "/translatorkit/index.html"
+  }
+];
