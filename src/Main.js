f2b8aa9468bc40da417859b8109eaf42fba8da55
Ibrahim-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-09-11 21:16:11 +0200

    add donate url

--- src/Main.js
+++ src/Main.js
@@ -27,6 +27,17 @@ const Main = () => (
                     <div className="overlay"></div>
                 </div>
                 <div className='side side-right'>
+                    <div className="donation">
+                        <div>
+                            <p>Donate for continuous development</p>
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick" />
+                                <input type="hidden" name="hosted_button_id" value="A8THPTEULHTHE" />
+                                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                                <img alt="" border="0" src="https://www.paypal.com/en_EG/i/scr/pixel.gif" width="1" height="1" />
+                            </form>
+                        </div>   
+                    </div>
                     <div className='intro-content'>
                         <div className='profile'>
                             <img src={black} alt='double-editor'></img>
